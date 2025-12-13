7ef71fd75af2c1f6736a85a0eb550449f44127d6
Shawna <shawnastaff@gmail.com>
2025-12-13 07:36:37 -0800

    Configure EAS build with Google services secret

---
+++ app.config.js
@@ -0,0 +1,37 @@
+export default {
+  expo: {
+    name: "Personal Budget",
+    slug: "personal-budgeting-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#0D0D0D"
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.staff.budgeting"
+    },
+    android: {
+      package: "com.staff.budgeting",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#0D0D0D"
+      },
+      edgeToEdgeEnabled: true,
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./google-services.json"
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      eas: {
+        projectId: "1e4cc392-7cc2-4b2f-91a4-b676d99b8cd4"
+      }
+    }
+  }
+};
